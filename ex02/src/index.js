836aa872d152d2aeae7c275bb92f81af3c5150b9
Anela Alic <ane@Tariks-MacBook-Air.local>
2021-03-10 19:51:39 +0100

    ex01 done

---
+++ ex02/src/index.js
@@ -0,0 +1,10 @@
+function main() [
+    // Ne mogu da vjerujem sta radim.
+    /* Ma sta je ovo vise ja se pogubih ziv, pobogu ste ljudi ljudski
+    da bi li ljudski
+    */
+
+]
+
+console.log(main());
+module.exports = main;
