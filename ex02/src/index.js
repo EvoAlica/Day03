c98fcc143a96c181bd903f6ed1acc8a7514098ac
Anela Alic <ane@Tariks-MacBook-Air.local>
2021-03-10 22:58:46 +0100

    Added ex05 folder

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,8 +1,12 @@
 function main() {
+// Only change code below this line
+
     // Ne mogu da vjerujem sta radim.
     /* Ma sta je ovo vise ja se pogubih ziv, pobogu ste ljudi ljudski
     da bi li ljudski
     */
+
+    // Only change code above this line
 }
 
 console.log(main());
--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,5 +1,8 @@
 function main() {
+    // Only change code below this line
     var forCat = 1;
+
+    // Only change code above this line
 }
 
 console.log(main());
