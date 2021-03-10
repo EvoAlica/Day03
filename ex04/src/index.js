8ee65935f0a7f4982edc1fa69246a0506d71c26d
Anela Alic <ane@Tariks-MacBook-Air.local>
2021-03-10 21:39:25 +0100

    Dodao status

---
+++ ex04/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+// Only change code below this line
+
+    var x = 10;
+    var y = 10;
+
+return [x, y];
+
+// Only change code above this line
+}
+
+console.log(main());
+module.exports = main;
